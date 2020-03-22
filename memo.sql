4ffbf0a2b9b57889dc6a4fb0a44a804fcd8ecb7d
yamada <z1ametapi@gmail.com>
2020-03-23 03:57:32 +0900

    データベース用意

---
+++ memo.sql
@@ -0,0 +1,9 @@
+create database memo_laravel character set utf8;;
+
+use memo_laravel
+
+create table memo (
+  id int not null auto_increment primary key,
+  title varchar(255),
+  memo varchar(255)
+);
