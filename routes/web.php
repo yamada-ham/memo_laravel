262accef5f6c447cbfb7e01ecc9716136bdaf849
yamada <z1ametapi@gmail.com>
2020-04-14 08:56:09 +0900

    ルーターの設定を追加

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,9 @@
 
 Route::get('/', 'IndexController@get');
 Route::post('/', 'IndexController@post');
+
+Route::get('/archive', 'IndexController@get');
+Route::post('/archive', 'IndexController@post');
+
+Route::get('/trash', 'IndexController@get');
+Route::post('/trash', 'IndexController@post');
